46f4e6ac5c019f1d6fa302f29d3392f21abde26c
Svyatoslav <reverse0@bk.ru>
2022-07-09 21:08:00 +0300

    Добавьте файлы проекта.

---
+++ TestAppWSS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestAppWSS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
